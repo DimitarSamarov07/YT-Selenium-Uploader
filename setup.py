9f9b152455bbdfa263b444cb7a716163e3e93da4
DimitarSamarov07 <samarovdimitar@gmail.com>
2023-01-18 15:24:01 +0200

    Upload project skeleton

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='YT-Selenium-Uploader',
+    version='1.0',
+    packages=['yt-selenium-uploader'],
+    install_requires=[
+        'undetected-chromedriver'
+    ],
+    license="MIT",
+    author="Dimitar Samarov",
+    url='https://github.com/DimitarSamarov07/YT-Selenium-Uploader',
+)
